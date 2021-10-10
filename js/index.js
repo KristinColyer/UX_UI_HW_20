19357cdca0a2547cd3ddecf74ff664a0c6927a2d
KristinColyer <82232102+KristinColyer@users.noreply.github.com>
2021-10-09 20:38:18 -0500

    update

---
+++ js/index.js
@@ -0,0 +1,21 @@
+ /* 1 carousel */
+ $('#carousel-1').carousel({
+    interval: 4000,
+    wrap: true,
+    keyboard: true
+});
+$('#carousel-2').carousel({
+    interval: 4000,
+    wrap: true,
+    keyboard: true
+});
+$('#carousel-3').carousel({
+    interval: 4000,
+    wrap: true,
+    keyboard: true
+});
+$('#carousel-4').carousel({
+    interval: 4000,
+    wrap: true,
+    keyboard: true
+});   
